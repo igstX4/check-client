f3f7f5019bc5062ce78283d9267ed44be9bffefa
igstX4 <programx41@gmail.com>
2025-01-15 23:54:19 +0200

    asdad

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000/api';
+const API_URL = 'https://check-server-pi.vercel.app/api';
 
 // Инстанс для клиентских запросов
 export const clientApi = axios.create({
