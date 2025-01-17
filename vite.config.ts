27fec193e2457704dc6b65c16a5318cd7c751a3e
igstX4 <programx41@gmail.com>
2025-01-17 14:58:10 +0200

    sadasd

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // Автоматическое обновление сервис-воркера
+      manifest: {
+        name: 'Check Platform',
+        short_name: 'Check Platform',
+        description: 'Check Platform',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          
+        ],
+      },
+    }),
+  ],
+});
